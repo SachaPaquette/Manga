37aabe28ed75f0c5c41779bb9a503800f6521e2c
sach <sachapaquette12@gmail.com>
2024-07-07 18:44:38 -0400

    Removed multiple unused config constants from the config file to be easier to navigate into, also cleaned driver config file

--- Driver/driver_config.py
+++ Driver/driver_config.py
@@ -5,9 +5,6 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 import random
 from Config.config import Config
-import random
-from Config.config import Config
-
 def driver_setup():
     # Set up the driver options
     options = Options()
@@ -16,8 +13,6 @@ def driver_setup():
     caps['goog:loggingPrefs'] = {'performance': 'ALL'}
     # Add caps to the options
     options.set_capability('goog:loggingPrefs', {'performance': 'ALL'})
-    # Keep the browser open after the script finishes executing (for debugging)
-    #options.add_experimental_option('detach', True)
     # Run in headless mode (without opening a browser window)
     #options.add_argument('--headless')
     # Disable logging (1: INFO, 2: WARNING, 3: ERROR)
@@ -25,8 +20,7 @@ def driver_setup():
     # Set a random user agent
     options.add_argument(f"user-agent={random.choice(Config.USER_AGENTS)}")
     # ChromeDriverManager will install the latest version of ChromeDriver
-    driver = webdriver.Chrome(service=Service(
-        ChromeDriverManager().install()), options=options)
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
     # put the browser in focus
     driver.switch_to.window(driver.current_window_handle)
     return driver
